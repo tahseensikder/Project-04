b74e6347198a8345730c24f14056517bf4cba044
tahseensikder <tahseensikder@gmail.com>
2022-02-19 00:12:32 +0600

    Header

--- js/app.js
+++ js/app.js
@@ -1,4 +1,14 @@
 // Preloader
 $(window).on('load',function(){
-    $('#preloader').fadeOut(3000)
+    $('#preloader').fadeOut(1000)
+})
+$(function(){
+    $(window).on('scroll',function(){
+        if ($(window).scrollTop()>$('#nav').height()){
+            $('#nav').addClass('menu_animate')
+        }
+        else {
+            $('#nav').removeClass('menu_animate')
+        }
+       })
 })
