f6a7b2289de3455b7acdf85608510358a68d1e1e
tahseensikder <tahseensikder@gmail.com>
2022-02-19 02:03:03 +0600

    banner

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ $(window).on('load',function(){
     $('#preloader').fadeOut(1000)
 })
 $(function(){
+    // fixed nav
     $(window).on('scroll',function(){
         if ($(window).scrollTop()>$('#nav').height()){
             $('#nav').addClass('menu_animate')
@@ -10,5 +11,13 @@ $(function(){
         else {
             $('#nav').removeClass('menu_animate')
         }
-       })
+    })
+    $('.banner_slider').slick({
+        prevArrow:'<i class="slider_icon fas fa-angle-left"></i>',
+        nextArrow:'<i class="slider_icon fas fa-angle-right"></i>',
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        speed: 1000,
+    })
 })
